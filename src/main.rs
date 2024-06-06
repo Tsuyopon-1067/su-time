409934c46db23a92ca5695b8c4b511135cfa3a49
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-06 15:52:21 +0900

    ✨ read yml file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod read_yml;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let time_table = read_yml::read_yml()?;
+
+    // 読み込んだ内容を表示する
+    for time_slot in time_table.time {
+        println!(
+            "{}: Start: {}, End: {}",
+            time_slot.class, time_slot.start, time_slot.end
+        );
+    }
+
+    Ok(())
 }
