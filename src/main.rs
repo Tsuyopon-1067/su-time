d108decc5ac817e4e83c1eaea5f3c9a79b96c8fc
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-06 15:16:46 +0900

    ✨ parse all option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("sutime")
+        .version("0.1.0")
+        .author("Tsuyopon_1067")
+        .about("show a timetable")
+        .arg(
+            Arg::with_name("all")
+                .short("a")
+                .help("print all timetable")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let all = matches.is_present("all");
+
+    println!("{}", all);
 }
